5134bbd5c7d3eb3a180af12a66b2b3e7fd7133d8
Peter Chen <peterchen2017125@gmail.com>
2022-12-28 21:44:32 +0800

    20221228

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,77 +1,71 @@
 import React from 'react';
-import Logo from './logo.png';
-import './App.css';
+import Logo from './logo.png'
 import {
   StyledAppHeader,
   StyledAppHeaderContent,
   StyledAppHeaderNavigationBar,
   StyledExploerButton,
   StyledSignInButton
-} from './App.Styled';
-import About from '../About';
-import Recipes from '../Recipes';
-import Cooking from '../Cooking';
+} from './App.Styled'
+import { Routes, Route, Link } from 'react-router-dom'
+import About from '../About'
+import Recipes from '../Recipes'
+import Cooking from '../Cooking'
 import Dinner from '../Dinner';
 
+function Index() {
+  return (
+    <React.Fragment>
+      <div className="content-title">Let's Get Cooking!</div>
+      <div className="content-text">
+        <div>Explore the best recipes from around the world.</div>
+        <div>Make cooking enjoyable again.</div>
+      </div>
+      <StyledExploerButton>Explore Recipes</StyledExploerButton>
+    </React.Fragment>
+  )
+}
+
 function App() {
-  // const [isSignInClick, setSignInClick] = React.useState(false)
-  const [pathname, setPathname] = React.useState('/')
   const [searchStart, setSearchStart] = React.useState(false)
+  const [pathname, setPathname] = React.useState('/')
 
   React.useEffect(() => {
     const pn = window.location.pathname
     setPathname(pn)
   }, [])
   return (
-
     <StyledAppHeader>
       <StyledAppHeaderNavigationBar>
         <div>
-          <a href='/'>
-            <img src={Logo} />
-          </a>
+          <a href="/"><img src={Logo} /></a>
         </div>
-        <div className='navigation-list'>
-          <div className='search'>
-            <i onClick={() => setSearchStart(!searchStart)} className='fas fa-search'></i>
-            <input className={searchStart ? 'open' : ''} placeholder="搜尋網站目標" type='text' />
+        <div className="navigation-list">
+          <div className="search">
+            <i onClick={() => setSearchStart(!searchStart)} className="fas fa-search"></i>
+            <input className={searchStart ? 'open' : ''} type="text" placeholder="搜尋網站目標" />
           </div>
-          <div className='navigation'>
+          <div className="navigation">
             <ul>
-              <li><a href='/recipes'>Recipes</a></li>
-              <li><a href="/dinner">Dinner TV</a></li>
-              <li><a href="/cooking">Cooking School</a></li>
-              <li><a href='/about'>About</a></li>
+              <li><Link to='/recipes'>Recipes</Link></li>
+              <li><Link to='/dinner'>Dinner TV</Link></li>
+              <li><Link to='/cooking'>Cooking School</Link></li>
+              <li><Link to='/about'>About</Link></li>
             </ul>
           </div>
           <div>
-            <StyledSignInButton>Sign Up</StyledSignInButton>
+            <StyledSignInButton>Sign In</StyledSignInButton>
           </div>
         </div>
       </StyledAppHeaderNavigationBar>
       <StyledAppHeaderContent>
-        {pathname === '/' &&
-          <React.Fragment>
-            <div className='content-title'>Let's Get Cooking</div>
-            <div className='content-text'>
-              <p>Explore the best recipes from around the world.</p>
-              <p>Make cooking enjoyble again.</p>
-            </div>
-            <StyledExploerButton>Explore Recipes</StyledExploerButton>
-          </React.Fragment>
-        }
-        {
-          pathname === '/about' && <About />
-        }
-        {
-          pathname === '/recipes' && <Recipes />
-        }
-        {
-          pathname === '/cooking' && <Cooking />
-        }
-        {
-          pathname === '/dinner' && <Dinner />
-        }
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dinner" element={<Dinner />} />
+          <Route path='/recipes' element={<Recipes />} />
+          <Route path='/cooking' element={<Cooking />} />
+        </Routes>
       </StyledAppHeaderContent>
     </StyledAppHeader>
   );
